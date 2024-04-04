8c535b950d5bc03feaf1a777de3ff1ca5135ce20
gabriel-rosa-tech <61751895+gabriel-rosa-tech@users.noreply.github.com>
2024-04-03 21:38:55 -0300

    add group

---
+++ pages/4_🧑‍🤝‍🧑_Grupo.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Sobre')
+st.subheader('Projeto desenvolvido por Estudantes da FIAP - Pós Tech em Data Analytics.')
+
+st.subheader('Github')
+st.write('https://github.com/almeida25/passos_magicos')
+
+st.subheader('Membros')
+st.markdown("<li>Barbara Campos</li> \
+        <li>Brendon Calazans</li>\
+        <li>Carlos Eduardo</li> \
+        <li>Gabriel Rosa</li>", unsafe_allow_html=True)
